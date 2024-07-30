78d1158e635dd05bc7ad7272608c1f9f4e02a429
Shayne999 <shaynejuniorn@gmail.com>
2024-07-30 15:24:34 +0200

    3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Flask App"""
+from flask import Flask
+from flask_babel import Babel
+from flask import render_template, request
+
+
+class Config:
+    """Config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """determine the best match with our supported languages"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/')
+def index() -> str:
+    """Returns the index page"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
