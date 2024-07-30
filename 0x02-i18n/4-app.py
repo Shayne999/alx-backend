09787aefb28f32175872140593c666847a69e525
Shayne999 <shaynejuniorn@gmail.com>
2024-07-30 17:05:58 +0200

    locale

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -24,10 +24,14 @@ def get_locale() -> str:
     """Resort to the previous default behavior if the
     selected locale is not supported.
     """
-    locale = request.args.get("locale")
-    if locale in app.config["LANGUAGES"]:
-        return locale
-
+    queries = request.query_string.decode('utf-8').split('&')
+    query_table = dict(map(
+        lambda x: (x if '=' in x else '{}='.format(x)).split('='),
+        queries,
+    ))
+    if 'locale' in query_table:
+        if query_table['locale'] in app.config["LANGUAGES"]:
+            return query_table['locale']
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
